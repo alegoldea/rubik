612972f9734795f02450e89f49c8d266f05c465a
alegoldea <62378466+alegoldea@users.noreply.github.com>
2021-05-23 15:13:00 +0300

    Add files via upload

---
+++ Assets/Zooming.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zooming : MonoBehaviour
+{
+    public float speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+    }
+}
